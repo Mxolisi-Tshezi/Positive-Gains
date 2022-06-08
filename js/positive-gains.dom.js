b44b5e0afe42f98d79fe3bc55257f52e260f9ad6
Wiseman Mabusela <wisemanwiseman217@gmail.com>
2022-06-08 16:28:30 +0200

    update

---
+++ js/positive-gains.dom.js
@@ -0,0 +1,9 @@
+const Calculate = document.querySelector('.Calculate');
+const mass = document.getElementById('.mass');
+const age = document.getElementById('.age');
+const height = document.getElementById('.height');
+
+Calculate.addEventListener('click', function(){
+
+
+} )
--- js/positive-gains.js
+++ js/positive-gains.js
@@ -1,3 +1,22 @@
 function positiveGains() {
-    
+    var massIn = 0;
+    var heightIn = 0;
+
+    function enterMass(myMass){
+        massIn = myMass
+
+    }
+    function enterHeight(myHeight){
+        heightIn = myHeight
+    }
+    function retrurnBMI(){
+         massIn/heightIn;
+    }
+    return{
+        enterHeight,
+        enterMass,
+        retrurnBMI
+
+    }
+
 }

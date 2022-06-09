b2dcaaea697fdb6fcce9ec4d89c2a5a6201fd061
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-06-09 13:31:14 +0200

    update

--- BMI-losing/js/positiveGains.dom.js
+++ BMI-losing/js/positiveGains.dom.js
@@ -1,10 +1,12 @@
 const calculateButtonForLoss = document.querySelector(".CalculateLoss");
-const massForLoss = document.getElementById(".massLoss");
-const heightForLoss = document.getElementById(".heightLoss");
+const massForLoss = document.getElementById("massLoss");
+const heightForLoss = document.getElementById("heightLoss");
 const DisplayForLoss = document.querySelector(".Text_BMILoss");
 
+const myPositiveGain = positiveGains1()
+
 calculateButtonForLoss.addEventListener("click", function(){
-    myPositiveGain.enterHeight(Number(heightForLoss.value));
-    myPositiveGain.enterMass(Number(massForLoss.value));
-    DisplayForLoss.innerHTML = myPositiveGain.returnBMI();
+    myPositiveGain.enterHeight1(Number(heightForLoss.value));
+    myPositiveGain.enterMass1(Number(massForLoss.value));
+    DisplayForLoss.innerHTML = myPositiveGain.returnBMI1();
 })

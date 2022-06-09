a0834f327b3910cd0cd7ef39a22f172da90e39d2
Tamia Quiyara <mia.mbuckingworth@gmail.com>
2022-06-09 11:27:12 +0200

    Added dom

--- js/positive-gains.dom.js
+++ js/positive-gains.dom.js
@@ -1,7 +1,7 @@
 const calculateButtonForGain = document.querySelector('.CalculateGain');
 const massForGain = document.getElementById('mass_Gain');
 const heightForGain = document.getElementById('height_Gain');
-const DisplayForGain = document.querySelector('.Text_BMIGain')
+const DisplayForGain = document.querySelector('.Text_BMIGain');
 
 const  myPositiveGain = positiveGains();
 
@@ -11,3 +11,14 @@ calculateButtonForGain.addEventListener('click', function(){
     DisplayForGain.innerHTML = myPositiveGain.returnBMI();
 
 } )
+
+const calculateButtonForLoss = document.querySelector(".CalculateLoss");
+const massForLoss = document.getElementById(".massLoss");
+const heightForLoss = document.getElementById(".heightLoss");
+const DisplayForLoss = document.querySelector(".Text_BMILoss");
+
+calculateButtonForLoss.addEventListener("click", function(){
+    myPositiveGain.enterHeight(Number(heightForLoss.value));
+    myPositiveGain.enterMass(Number(massForLoss.value));
+    DisplayForLoss.innerHTML = myPositiveGain.returnBMI();
+})
